f67ca78d4b92a50a097271fd42c8a8efbd4fdc13
Kirill <kirashuvalov@rambler.ru>
2021-03-04 22:20:51 +0300

    Intinal commit

--- addresbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/TestBase.java
+++ addresbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/TestBase.java
@@ -4,16 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addresbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
     FirefoxDriver wd;
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
         app.init();
     }
@@ -26,7 +26,7 @@ public class TestBase {
         wd.findElement(By.linkText("add new")).click();
     }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
     }
